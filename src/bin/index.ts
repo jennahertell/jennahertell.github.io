662b9a58788f794795dc3047723981d4cd09b0d6
Eero Uusaro <eerouusaro@gmail.com>
2025-04-03 01:40:01 +0300

    First version

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,6 +1,6 @@
 import server from "../server";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
     console.log(`Server listening to PORT ${PORT}`);
